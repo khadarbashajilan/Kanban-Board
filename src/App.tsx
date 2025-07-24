c5d2da18d7346230e3077076b9d996c36c8dd5b9
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-07-24 21:32:52 +0530

    implemented routing paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-
-import './index.css'
+import Layout from "./layouts/Layout";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const routes = [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <NotFoundPage />,
+      children: [
+        {
+          path: "/",
+          element: <BoardsList />,
+        },
+        {
+          path: "/boards",
+          element: <BoardsList />,
+        },
+        {
+          path: "/boards/:boardId",
+          element: <BoardDetails />,
+        },
+        {
+          path: "/boards/:boardId/card/:cardId",
+          element: <CardDetails />,
+        },
+      ],
+    },
+  ];
 
+  const router = createBrowserRouter(routes);
 
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
